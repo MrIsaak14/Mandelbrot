3ab167b87e1bb0c5f44686ef94cb41020a358384
Tony Oelrich <tonyoelrich8@gmail.com>
2024-09-18 14:21:55 +0200

    tkr

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 print('hello')
 
-#heyyyyyyyy
+
+from tkinter import Frame
+
+scherm=Frame()
+
+scherm.configure(background="blue")
+scherm.configure(width=200, height=100)
+scherm.master.title("mandelbrot")
+scher.pack()
+
+
+
+
+
+scherm.mainloop()
