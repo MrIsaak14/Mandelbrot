0af33a2a33397422c74354137cc4a7ee712681f4
Tony Oelrich <tonyoelrich8@gmail.com>
2024-09-18 14:33:25 +0200

    import dingen

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-print('hello')
-
-
+import math
 from tkinter import Frame
+from tkinter import Label, Button, Entry
+
+s
 
 scherm=Frame()
 
 scherm.configure(background="blue")
 scherm.configure(width=200, height=100)
 scherm.master.title("mandelbrot")
-scher.pack()
+scherm.pack()
+
+knop=Button(scherm)
+
+knop.configure(text="Start")
+knop.place(x=100, y=10)
+
+invoer=Entry(scherm)
+invoer.place(x=150, y=70)
 
 
 
