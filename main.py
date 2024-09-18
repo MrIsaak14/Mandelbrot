c744b5f13cb6aaf52e9716b2535a0d020270c1a1
Tony Oelrich <tonyoelrich8@gmail.com>
2024-09-18 14:58:46 +0200

    alles

--- main.py
+++ main.py
@@ -2,22 +2,65 @@ import math
 from tkinter import Frame
 from tkinter import Label, Button, Entry
 
-s
-
 scherm=Frame()
 
-scherm.configure(background="blue")
-scherm.configure(width=200, height=100)
+scherm.configure(background="light grey")
+scherm.configure(width=1000, height=700)
 scherm.master.title("mandelbrot")
 scherm.pack()
 
 knop=Button(scherm)
 
 knop.configure(text="Start")
-knop.place(x=100, y=10)
+knop.place(x=10, y=130)
 
-invoer=Entry(scherm)
-invoer.place(x=150, y=70)
+#invoor midden X
+invoerX=Entry(scherm)
+invoerX.place(x=90, y=10)
+uitvoer=Label(scherm)
+uitvoer.configure(text="Midden X:")
+uitvoer.place(x=10, y=10)
+
+
+#invoer midden Y
+invoerY=Entry(scherm)
+invoerY.place(x=90, y=40)
+uitvoer=Label(scherm)
+uitvoer.configure(text="Midden Y:")
+uitvoer.place(x=10, y=40)
+
+
+#invoer schaal
+invoerS=Entry(scherm)
+invoerS.place(x=90, y=70)
+uitvoer=Label(scherm)
+uitvoer.configure(text="Schaal:")
+uitvoer.place(x=10, y=70)
+
+
+#invoer max aantal
+invoerA=Entry(scherm)
+invoerA.place(x=90, y=100)
+uitvoer=Label(scherm)
+uitvoer.configure(text="Max aantal:")
+uitvoer.place(x=10, y=100)
+
+
+#functie mandelbrot
+
+a=invoerX
+b=invoerY
+
+mand=0
+
+for mand in range(mand < invoerA and )
+    
+    
+    
+ 
+    
+    
+
 
 
 
